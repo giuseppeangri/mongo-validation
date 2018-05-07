4a6ddf91a2948b58ad8dbfd9af0d31c4298a6d39
Giuseppe Angri <giuseppeangri@icloud.com>
2018-05-07 16:47:18 +0200

    Add custom field feature

--- mongo-validation.js
+++ mongo-validation.js
@@ -26,14 +26,14 @@ function mongoValidation() {
 
         return function(request, response, next) {
 
-            var paramValue = request.params[paramName];
+            var fieldValue = request.params[paramName];
 
-            if(isValidId(paramValue)) {
+            if(isValidId(fieldValue)) {
                 next();
             }
             else {
                 response.status(400).send({
-                    message : 'The param: \''+paramName+'\' with value \''+paramValue+'\' is not a valid ID.'
+                    message : 'The param \''+paramName+'\' with value \''+fieldValue+'\' is not a valid ID.'
                 });
             }
 
@@ -41,12 +41,15 @@ function mongoValidation() {
 
     }
 
-    function checkExist(mongoId, modelName) {
+    function checkExist(modelName, fieldName, fieldValue) {
 
         var promise = new Promise(function(resolve, reject) {
 
+            var conditions = {};
+            conditions[fieldName] = fieldValue;
+
             mongoose.model(modelName)
-                .findById(mongoId)
+                .findOne(conditions)
                 .exec()
                 .then(function(result) {
                     resolve(result);
@@ -67,50 +70,54 @@ function mongoValidation() {
 
         return function(request, response, next) {
 
-            var paramValue = request.params[paramName];
-
-            if(isValidId(paramValue)) {
-
-                checkExist(paramValue, modelName)
-                    .then(function(result) {
-                        
-                        if(result) {
-
-                            var appendToRequest = (options.appendToRequest === false) ? false : true;
-                            var aggregate       = (options.aggregate === false) ? false : true;
-                            var aggregateName   = (options.aggregateName && (typeof options.aggregateName === 'string') ) ? options.aggregateName : 'data';
-                            var objectName      = (options.objectName && (typeof options.objectName === 'string') ) ? options.objectName : modelName.toLowerCase();
-
-                            if(appendToRequest) {
-                                
-                                if(aggregate && !request[aggregateName]) {
-                                    request[aggregateName] = {};
-                                }
-
-                                (aggregate) ? request[aggregateName][objectName] = result : request[objectName] = result;
-
-                            }
-
-                            next();
-
-                        }
-                        else {
-                            response.status(400).send({
-                                message : 'The resource: \''+modelName+'\' with ID \''+paramValue+'\' does not exist.'
-                            });
-                        }
-
-                    })
-                    .catch(function(error) {
-                        response.status(500).send(error);
-                    });
-
-            }
-            else {
-                response.status(400).send({
-                    message : 'The param: \''+paramName+'\' with value \''+paramValue+'\' is not a valid ID.'
+            if(!paramName || !modelName) {
+                return response.status(500).send({
+                    message : 'Missing parameters in the middleware.'
                 });
             }
+
+            var fieldName  = (options.fieldName && (typeof options.fieldName === 'string') ) ? options.fieldName : '_id';
+            var fieldValue = request.params[paramName];
+            
+            if( (fieldName === '_id') && !isValidId(fieldValue) ) {
+                return response.status(400).send({
+                    message : 'The param \''+paramName+'\' with value \''+fieldValue+'\' is not a valid ID.'
+                });
+            }
+
+            checkExist(modelName, fieldName, fieldValue)
+                .then(function(result) {
+                    
+                    if(result) {
+
+                        var appendToRequest = (options.appendToRequest === false) ? false : true;
+                        var aggregate       = (options.aggregate === false) ? false : true;
+                        var aggregateName   = (options.aggregateName && (typeof options.aggregateName === 'string') ) ? options.aggregateName : 'data';
+                        var objectName      = (options.objectName && (typeof options.objectName === 'string') ) ? options.objectName : modelName.toLowerCase();
+
+                        if(appendToRequest) {
+                            
+                            if(aggregate && !request[aggregateName]) {
+                                request[aggregateName] = {};
+                            }
+
+                            (aggregate) ? request[aggregateName][objectName] = result : request[objectName] = result;
+
+                        }
+
+                        next();
+
+                    }
+                    else {
+                        response.status(404).send({
+                            message : 'The resource \''+modelName+'\' with the field \''+fieldName+'\' having value \''+fieldValue+'\' does not exist.'
+                        });
+                    }
+
+                })
+                .catch(function(error) {
+                    response.status(500).send(error);
+                });
 
         }
 
